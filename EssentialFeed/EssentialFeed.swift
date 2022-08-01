d70c39d1e1be5faecbf61e69d9614aef27b9c4fd
srijan <sribhati@adobe.com>
2022-08-01 22:11:58 +0530

    Create FeedItem Model.

---
+++ EssentialFeed/EssentialFeed.swift
@@ -0,0 +1,15 @@
+//
+//  EssentialFeed.swift
+//  EssentialFeed
+//
+//  Created by Srijan Bhatia on 01/08/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
