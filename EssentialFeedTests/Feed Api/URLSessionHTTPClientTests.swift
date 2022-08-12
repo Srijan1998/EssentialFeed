ed5d7330f3b800b31543c21bcfd04e3ef99fcc30
srijan <sribhati@adobe.com>
2022-08-12 23:30:00 +0530

    Added nil data and nil response to the stub to make the tests more explicit.

--- EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
@@ -30,7 +30,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         URLProtocolStub.startInterceptingRequests()
         let url = URL(string: "https://a-url.com")!
         let error = NSError(domain: "any error", code: 1)
-        URLProtocolStub.stub(url: url, error: error)
+        URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
         let sut = URLSessionHTTPClient()
         let expectation = expectation(description: "Wait for completion")
         sut.get(from: url) { result in
@@ -54,12 +54,14 @@ class URLSessionHTTPClientTests: XCTestCase {
         private static var stubs = [URL: Stub]()
         
         private struct Stub {
+            let data: Data?
+            let response: URLResponse?
             let error: NSError?
         }
         
         
-        static func stub(url: URL, error: NSError) {
-            stubs[url] = Stub(error: error)
+        static func stub(url: URL, data: Data?, response: URLResponse?, error: NSError?) {
+            stubs[url] = Stub(data: data, response: response, error: error)
         }
         
         static func startInterceptingRequests() {
@@ -83,6 +85,14 @@ class URLSessionHTTPClientTests: XCTestCase {
         
         override func startLoading() {
             guard let url = request.url, let stub = URLProtocolStub.stubs[url] else { return }
+            
+            if let data = stub.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            if let response = stub.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
             
             if let error = stub.error {
                 client?.urlProtocol(self, didFailWithError: error)
