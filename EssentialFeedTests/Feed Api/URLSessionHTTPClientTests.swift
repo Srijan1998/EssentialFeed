0d5e12ab0eca6e70cade6375ea2f320f66e31a7e
srijan <sribhati@adobe.com>
2022-08-13 10:47:59 +0530

    Move URL creation to a shared factory method.

--- EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
@@ -40,7 +40,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     
     func test_getFromURL_makesGetRequestWithURL() {
-        let url = URL(string: "https://a-url.com")!
+        let url = anyURL()
         let sut = makeSUT()
         let expectation = expectation(description: "Wait for request")
         URLProtocolStub.observeRequests { request in
@@ -56,7 +56,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_failsOnRequestError() {
-        let url = URL(string: "https://a-url.com")!
+        let url = anyURL()
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
         let sut = makeSUT()
@@ -82,6 +82,10 @@ class URLSessionHTTPClientTests: XCTestCase {
         let sut = URLSessionHTTPClient()
         trackMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
     }
     
     private class URLProtocolStub: URLProtocol {
