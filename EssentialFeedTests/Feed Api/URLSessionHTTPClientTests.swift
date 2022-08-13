a33c2dd707d1eb512b2d7c1ee9499d766c305fa8
srijan <sribhati@adobe.com>
2022-08-13 10:41:06 +0530

    Move the URLSessionHTTPClient(the sut) to a factory method.

--- EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
@@ -41,7 +41,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     func test_getFromURL_makesGetRequestWithURL() {
         let url = URL(string: "https://a-url.com")!
-        let sut = URLSessionHTTPClient()
+        let sut = makeSUT()
         let expectation = expectation(description: "Wait for request")
         URLProtocolStub.observeRequests { request in
             XCTAssertEqual(request.url, url)
@@ -59,7 +59,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         let url = URL(string: "https://a-url.com")!
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
-        let sut = URLSessionHTTPClient()
+        let sut = makeSUT()
         let expectation = expectation(description: "Wait for completion")
         sut.get(from: url) { result in
             switch result {
@@ -76,6 +76,11 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
     
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
